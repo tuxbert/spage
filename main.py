73dbf06ffe4e719786d2c3a74bd895f17e936ac4
Elliot Paton-Simpson <tuxbert>
2018-09-17 16:18:31 +1200

    Created SPAGE (SPAGE Python Adventure Game Engine)

---
+++ main.py
@@ -0,0 +1,33 @@
+'''
+Welcome to SPAGE (SPAGE Python Adventure Game Engine)
+
+SPAGE is a library that works in conjunction with Pygame to allow a user to
+create and package adventure games using Python. It offers various
+functionality relating to characters, objects, scenery, dialogue, areas, etc.
+
+'''
+
+import pygame
+
+areas = []
+
+class Area:
+    """
+    Consists of various planes on which the user may exist.
+    """
+    def __init__(self, name="Lab"):
+        """
+        Creates an area.
+        """
+        self.name = name
+    
+    def __str__(self):
+        """
+        Returns the area name.
+        """
+        return self.name
+
+
+areas = [Area(), Area("Room")]
+
+[print(x) for x in areas]
