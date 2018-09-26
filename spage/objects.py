ff38b1652ebf8964b50c046414315104e3f2a5e4
Elliot Paton-Simpson <tuxbert>
2018-09-27 10:31:50 +1200

    Made certain kwargs mandatory, using sets to raise an appropriate error if needed.

--- spage/objects.py
+++ spage/objects.py
@@ -35,9 +35,17 @@ class Character:
         """
         Creates a character.
         """
+        # Checks that no argument failed to be created.
+        mandatory_arguments = {'character_id', 'name', 'area', 'img_path'}
+        keys = set(kwargs.keys())
+        if not mandatory_arguments.issubset(keys):
+            first_missing = next(iter(mandatory_arguments.difference(keys)))
+            raise TypeError(f'Missing argument: {first_missing}')
+        
         self.id = kwargs['character_id']
         self.name = kwargs['name']
         self.area = kwargs['area']
+        self.img_path = kwargs['img_path']
     
     def __str__(self):
         """
@@ -61,7 +69,7 @@ def find_character(character_id):
         if character.id == character_id:
             return character
     
-    raise ValueError(f"No Character defined with id: {character_id}")
+    raise ValueError(f'No Character defined with id: {character_id}')
 
 def character_exists(character_id):
     """
@@ -96,14 +104,13 @@ def find_area(area_id):
         if area.id == area_id:
             return area
     
-    raise ValueError(f"No Area defined with id: {area_id}")
+    raise ValueError(f'No Area defined with id: {area_id}')
 
 def area_exists(area_id):
     """
     Returns a boolean corresponding to the area_id provided.
     """
     for area in areas:
-        print(area.id)
         if area.id == area_id:
             return True
     
@@ -114,7 +121,7 @@ def create_area(area_id, **kwargs):
     Adds an area to the list of areas.
     """
     if area_exists(area_id):
-        raise ValueError(f"Area with id {area_id} already created.")
+        raise ValueError(f'Area with id {area_id} already created.')
     
     new_area = {
         'area_id': area_id,
